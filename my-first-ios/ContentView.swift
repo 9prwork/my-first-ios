7f39baca099f9a2d7e46685b54e27d2cdc87ea64
Prite <9pr.work@gmail.com>
2026-03-24 10:01:21 +0700

    Initial Commit

---
+++ my-first-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-first-ios
+//
+//  Created by Entronica on 24/3/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-first-ios/my_first_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_iosApp.swift
+//  my-first-ios
+//
+//  Created by Entronica on 24/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct my_first_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
